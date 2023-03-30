17ef20d08d4aac5cecad0e0ff200f0feace4ed0a
hepno <me@hepno.dev>
2023-03-30 19:13:03 +0200

    add(Command): add custom command gist

---
+++ src/main/java/dev/hepno/gemsplugin/GemsPlugin.java
@@ -0,0 +1,17 @@
+package dev.hepno.gemsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GemsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
