fb15c7b88102887fe1ae65507059851a1e362881
hepno <me@hepno.dev>
2023-03-30 19:17:51 +0200

    add(pom.xml): add placeholderapi dependency

--- src/main/java/dev/hepno/gemsplugin/GemsPlugin.java
+++ src/main/java/dev/hepno/gemsplugin/GemsPlugin.java
@@ -1,13 +1,13 @@
 package dev.hepno.gemsplugin;
 
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GemsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // PlaceholderExpansion expansion = new GemsExpansion();
     }
 
     @Override
